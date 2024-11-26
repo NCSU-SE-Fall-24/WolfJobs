e08af400b563aecdb4979522d41d18d6ceed2d54
Jap Purohit <pjap041@gmail.com>
2024-11-25 21:25:09 -0500

    fix: remove sensitive information logging in mongoose.js

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(`mongodb+srv://kpatel47:${process.env.MONGO_PASS}@wolfjobs.0qqf1.mongodb.net/wolf`);
-
+console.log(process.env.MONGO_PASS);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to mongodb'));
